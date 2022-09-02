c418895a14fdfbb9f511513bc43828cc0253a7b4
Angel Castro <angelcastro.pr@gmail.com>
2022-09-02 00:33:57 -0400

    made some api implementations

--- objects-shots/js/main.js
+++ objects-shots/js/main.js
@@ -2,7 +2,14 @@
 fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-      console.log(data.drinks)
+      console.log(data.drinks[1])
+      document.querySelector('h2').innerText = data.drinks[1].strDrink
+      document.querySelector('img').src = data.drinks[1].strDrinkThumb
+      document.querySelector('.ingredients1').innerText = data.drinks[1].strIngredient1
+      document.querySelector('.ingredients2').innerText = data.drinks[1].strIngredient2
+      document.querySelector('.ingredients3').innerText = data.drinks[1].strIngredient3
+      document.querySelector('.ingredients4').innerText = data.drinks[1].strIngredient4
+      document.querySelector('h3').innerText = data.drinks[1].strInstructions
     })
     .catch(err => {
         console.log(`error ${err}`)
